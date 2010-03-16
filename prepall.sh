0e37859e2a48a991aa38b9881d0691b4fbc09c62
James Slagle <slagle@rpath.com>
2010-03-16 14:57:47 -0400

    Add smartform to prepall.sh

--- prepall.sh
+++ prepall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd `dirname $0`
-for x in conary conary-test catalog-service mint rpath-capsule-indexer rpath-product-definition rpath-storage rpath-xmllib
+for x in conary conary-test catalog-service mint rpath-capsule-indexer rpath-product-definition rpath-storage rpath-xmllib smartform/py/smartform
 do
     make -C $x
 done
