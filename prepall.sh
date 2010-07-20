05e5e02d0ac36f21b3c5b66c74ce9e70665229fb
Ben Rector <https://issues.rpath.com/>
2010-07-20 13:06:37 -0400

    Backed out changeset 54359e5ad6b7

--- prepall.sh
+++ prepall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd `dirname $0`
-for x in conary conary-test catalog-service mint rmake rpath-capsule-indexer rpath-models rpath-product-definition rpath-repeater rpath-storage rpath-xmllib smartform/py/smartform raa
+for x in conary conary-test catalog-service mint rmake rpath-capsule-indexer rpath-models rpath-product-definition rpath-storage rpath-xmllib smartform/py/smartform raa
 do
     make -C $x
 done
