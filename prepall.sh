776227c3b0b84036dae9cffde648d1164b815202
Michael Tharp <mtharp@rpath.com>
2010-11-04 13:45:26 -0400

    add raa and robj include symlinks

--- prepall.sh
+++ prepall.sh
@@ -2,6 +2,6 @@
 cd `dirname $0`
 for x in conary conary-test catalog-service mint rmake rpath-capsule-indexer rpath-models rpath-product-definition rpath-repeater rpath-storage rpath-xmllib smartform/py/smartform raa
 do
-    make -C $x
+    make -C $x || exit 1
 done
-make -C conary minimal
+make -C conary minimal || exit 1
