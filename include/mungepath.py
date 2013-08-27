721c8cae28244f35e9fa9ea2ba916cf111953c68
Michael Tharp <michael.tharp@sas.com>
2013-08-27 14:24:59 +0000

    make mungepath override virtualenvs

--- include/mungepath.py
+++ include/mungepath.py
@@ -1,7 +1,23 @@
 import os
 import sys
 def insert(path):
-    index = max([0]+[
-        sys.path.index(x) for x in os.getenv('PYTHONPATH','').split(os.pathsep)
-        if x in sys.path])
-    sys.path.insert(index + 1, path)
+    """
+    Insert C{path} at the front of sys.path, but after $PYTHONPATH.
+
+    The goal is to override installed system packages and virtualenv packages
+    but not anything specified via environment like the command wrappers you get
+    running e.g. ./bin/conary
+    """
+    new = []
+    for pp in os.getenv('PYTHONPATH','').split(os.pathsep):
+        if not pp:
+            continue
+        if pp in sys.path:
+            sys.path.remove(pp)
+        pp = os.path.abspath(pp)
+        if pp in sys.path:
+            sys.path.remove(pp)
+        if pp not in new:
+            new.append(pp)
+    new.append(path)
+    sys.path = new + sys.path
