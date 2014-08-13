8bc186ca79c12ef84c80c9ee4fdaae1521bac130
Walter Scheper <Walter.Scheper@sas.com>
2014-08-13 15:30:07 -0400

    Update devrbuilder.sh script and associated system model
    System model changes:
    * removed mercurial and hg-forest
    * added git and forester
    
    Script changes:
    * Added type argument for building a dev appengine vapp
    * clone forest using forester and link it into place
    * updated label used for group-rpath-platform and group-rbuilder-dist
    * for vapp appengines, use curl to set the sas authorized key and then
      copy it to root user

--- devrbuilder.sh
+++ devrbuilder.sh
@@ -6,16 +6,23 @@
 # install any needed dev tools.
 #
 
-echo Someone ought to fix this script
-exit 1
-
-if [ ! $# -eq 2 ] ; then
-    echo "usage: $0 <hostname> <rbuilder branch>"
+if [ $# -lt 2 ] ; then
+    echo "usage: $0 <hostname> <rbuilder branch> [type]"
     exit 1
 fi
 
 hostname=$1
 branch=$2
+if [ -z $3 ] ; then
+    group_type="appliance"
+else
+    group_type=$3
+fi
+
+if [ "$group_type" != "appliance" -a "$group_type" != "vapp" ] ; then
+    echo "type must be appengine or vapp"
+    exit 1
+fi
 
 local_checkout=$(dirname $0)
 
@@ -42,14 +49,23 @@ function copy() {
 }
 
 # Deploy my ssh key
-ssh-copy-id root@$hostname
-
-# Copy my known hosts to avoid asking later
-copy ~/.ssh/known_hosts /root/.ssh/known_hosts
-
+#if [ "$group_type" == "vapp" ] ; then
+#    if [ -e ~/.ssh/id_*.pub ] ; then
+#        curl -k -F 'files=@~/.ssh/id_*.pub' https://$hostname/SASvAppLedger/authorizedKeys
+#    else
+#        curl -k -F "files=$(ssh-add -L | head -n1)" https://$hostname/SASvAppLedger/authorizedKeys
+#    fi
+#    ssh -t -A sas@$hostname "su -c 'cp -R /home/sas/.ssh /root/.'"
+#else
+#    ssh-copy-id root@$hostname
+#fi
+#
+## Copy my known hosts to avoid asking later
+#copy ~/.ssh/known_hosts /root/.ssh/known_hosts
+#
 # Get the latest versions of group rbuilder
-platform=$(conary rq --labels group-rpath-platform=newton.eng.rpath.com@rpath:platform-$branch-devel)
-dist=$(conary rq --labels group-rbuilder-dist=newton.eng.rpath.com@rpath:rba-$branch-rba)
+platform=$(conary rq --labels group-rpath-platform=newton.eng.rpath.com@sas:platform-$branch-devel)
+dist=$(conary rq --labels group-rbuilder-dist=newton.eng.rpath.com@sas:rba-$branch-rba)
 
 tmpcnyconf=$(mktemp)
 cat << EOF >$tmpcnyconf
@@ -63,7 +79,11 @@ EOF
 
 copy $tmpcnyconf /etc/conary/config.d/httpProxy
 
-cat $local_checkout/rbuilder-system-model | sed -e "s|^search\ group-rpath-platform.*|search\ $platform|" | sed -e "s|^search\ group-rbuilder-dist.*|search\ $dist|" > /tmp/rbuilder-system-model
+cat $local_checkout/rbuilder-system-model | \
+    sed -e "s|^search\ group-rpath-platform.*|search\ $platform|" | \
+    sed -e "s|^search\ group-rbuilder-dist.*|search\ $dist|" | \
+    sed -e "s|^install\ group-rbuilder-.*|install\ group-rbuilder-$group_type|" > \
+    /tmp/rbuilder-system-model
 
 # Configure conary to use rbuilder.unx as a proxy
 run "echo 'includeConfigFile http://delphi.unx.sas.com/conaryrc' > /etc/conary/config.d/proxy"
@@ -74,7 +94,7 @@ run echo 'updating system'
 run conary updateall
 
 # Write out my hgrc
-copy ~/.hgrc /root/.hgrc
+copy ~/.gitconfig /root/.gitconfig
 
 # Copy vim config
 copy ~/.vimrc /root/.vimrc
@@ -84,9 +104,12 @@ copy ~/.vim /root/.vim
 checkoutdir="/srv/code/products/rbuilder"
 checkout="$checkoutdir/$branch"
 run mkdir -p $checkoutdir
-run hg fclone ssh://$USER@scc.unx.sas.com//hg/products/rbuilder/$branch $checkout
-run make -C $checkout
-run make -C $checkout install-pth
+#run hg fclone ssh://$USER@scc.unx.sas.com//hg/products/rbuilder/$branch $checkout
+run forester init --branch $branch --wms --wmsbase http://wheresmystuff.unx.sas.com --wmspath scc/appengine --subdir /root/git --forest appengine
+run forester clone appengine
+run ln -sf /root/git/scc/appengine $checkout
+run make -C $checkout/appengine
+run make -C $checkout/appengine install-pth
 
 # Setup python config
 if [ -f ~/.pystartup ] ; then
